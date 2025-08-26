5709075d45117950f0ba8bdc6c9ca5201e8ff626
TE24b-jack-carlsund <jack.carlsund@elev.ga.ntig.se>
2025-08-26 14:30:23 +0200

    Update Program.cs
    updated

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,17 +8,17 @@
 // Console.ReadLine();
 
 
-//string verb;
-// string name;
-// string fluid;
-// Console.WriteLine("Whats your name?");
-// name = Console.ReadLine();
-// Console.WriteLine("Name a fluid");
-// fluid = Console.ReadLine();
-// Console.WriteLine("Name a verb");
-// verb = Console.ReadLine();
-// Console.WriteLine($"Hello! Your name is {name} and you like {fluid}. You also like to {verb}.");
-// Console.ReadLine();
+string verb;
+ string name;
+ string fluid;
+ Console.WriteLine("Whats your name?");
+ name = Console.ReadLine();
+ Console.WriteLine("Name a fluid");
+ fluid = Console.ReadLine();
+ Console.WriteLine("Name a verb");
+ verb = Console.ReadLine();
+ Console.WriteLine($"Hello! Your name is {name} and you like {fluid}. You also like to {verb}.");
+ Console.ReadLine();
 
 
 
