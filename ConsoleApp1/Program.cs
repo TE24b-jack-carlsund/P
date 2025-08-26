b9ec18fb0f044c8149cc38c5502fc930008b1b57
TE24b-jack-carlsund <jack.carlsund@elev.ga.ntig.se>
2025-08-26 14:14:46 +0200

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿// string name;
+// Console.WriteLine("Vad heter du?");
+// name = Console.ReadLine();
+// string positivtOrd = "snälla";
+// Console.WriteLine("Hello TE24b!");
+// //Console.WriteLine("Idag tycker " + name + " att ni är " + positivtOrd + "idag!");
+// Console.WriteLine($"Idag tycker {name} att ni är {positivtOrd} idag!");
+// Console.ReadLine();
+
+
+//string verb;
+// string name;
+// string fluid;
+// Console.WriteLine("Whats your name?");
+// name = Console.ReadLine();
+// Console.WriteLine("Name a fluid");
+// fluid = Console.ReadLine();
+// Console.WriteLine("Name a verb");
+// verb = Console.ReadLine();
+// Console.WriteLine($"Hello! Your name is {name} and you like {fluid}. You also like to {verb}.");
+// Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
